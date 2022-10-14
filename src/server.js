ab4707f61eec5a90b0e13389af9cbb02fca68ce5
pooh <cktjdgus45@naver.com>
2022-10-14 21:55:12 +0900

    set pug on express view engine

--- src/server.js
+++ src/server.js
@@ -1,8 +1,24 @@
 import express from 'express';
+import path from 'path';
+
+
 
 const PORT_NUM = 3000;
 
 const app = express();
+const __dirname = path.resolve();
+console.log(__dirname);
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+
+app.use("/public", express.static(__dirname + "/src/public/"));
+
+app.get("/", (req, res, next) => {
+    res.render("home");
+})
+
+
 
 app.listen(PORT_NUM, () => {
     console.log(`server listening on ${PORT_NUM}`);
